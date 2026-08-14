ba417e4cc453ffe8cb9760b38ffd95f9f94b8b65
HETAXITAPODHAN29 <hetaxi.tapodhan@gmail.com>
2026-08-14 23:22:22 +0530

    upadated CreateDispatachModal

--- src/components/CreateDispatchModal.jsx
+++ src/components/CreateDispatchModal.jsx
@@ -1,12 +1,9 @@
 import { FaTimes } from "react-icons/fa";
 
 export default function CreateDispatchModal({
-  isOpen,
   onClose,
   onCreate,
 }) {
-  // If modal is not open, show nothing
-  if (!isOpen) return null;
 
   const handleSubmit = (e) => {
     e.preventDefault();
