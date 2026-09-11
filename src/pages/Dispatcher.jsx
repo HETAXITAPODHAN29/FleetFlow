e4eceaf773616dc3d1a9a5d2b3f63d139b800a12
HETAXITAPODHAN29 <hetaxi.tapodhan@gmail.com>
2026-09-11 23:24:49 +0530

    Updated dashboard

--- src/pages/Dispatcher.jsx
+++ src/pages/Dispatcher.jsx
@@ -21,15 +21,7 @@ import dispatches from "../data/dispatches";
 export default function Dispatcher() {
   // -------------------- States --------------------
 
-  const [search, setSearch] = useState("");
 
-  const [dispatchList, setDispatchList] = useState(dispatches);
-
-  const [selectedTrip, setSelectedTrip] = useState(null);
-
-  const [editingTrip, setEditingTrip] = useState(null);
-
-  const [deleteTrip, setDeleteTrip] = useState(null);
 
   const [showCreateModal, setShowCreateModal] = useState(false);
 
